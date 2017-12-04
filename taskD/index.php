0f623ade6a1b91cbb7ae60ac4dec51ad0e0b9775
Falua Temitope Oyewole <emmat0616@gmail.com>
2017-12-04 19:27:52 +0100

    TaskC done!!!! Yebaaaaaa

--- taskD/index.php
+++ taskD/index.php
@@ -1,27 +1,35 @@
 <?php
-function getSum($num){
-//    echo $num."\n";
-    $len = strlen($num);
-//    echo $len."\n";
-    $new_num = 0;
-    for ($i = 0; $i < $len; $i++ ) {
-        echo $i."\n";
-        $new_num = $new_num +$num[$i];
+
+
+function super_digit($sum_of_string){
+    $sum_of_string = (string)$sum_of_string;
+    $sum = 0;
+    for($i = 0; $i < strlen($sum_of_string); $i++) {
+        $sum += $sum_of_string[$i];
+        echo $sum."\n";
     }
-    return $new_num;
+    if(strlen($sum) > 1) {
+        super_digit($sum);
+    }//else return $sum;
 }
-$n = 148;
+
+$num = 148;
 $k = 3;
-$p = 0;
-$new = str_repeat($n, $k);
-$len = strlen($new);
-//echo $len;
-$p = getSum($new);
-$new_p = getSum($p);
-//while($p){
-//    if(strlen($p) == 1){
-//        break;
-//    }
-//    $p = getSum($p);
-//    echo $p;
-//}
+$str_to_num = (string)$num;
+$sum_of_string = str_repeat($str_to_num, $k);
+super_digit($sum_of_string);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
